c861ddbbab48ab5f1aa62ea98aa1b8021365375f
NathanKaul <nathankaul@ufl.edu>
2024-04-11 11:09:34 -0400

    Update
    Final encode and main

--- Main.py
+++ Main.py
@@ -1,7 +1,34 @@
 # This is Nathans code
 
 def encode(password):
-    pass
+    empty = ''
+    for i in password:
+        y = 3
+        if int(i) > 6:
+            x = (int(i) + 3) % 10
+        else:
+            x = int(i) + y
+        empty = empty + str(x)
+    return empty
+print(encode('00009962'))
 
 if __name__ == '__main__':
-    pass
+    menu = """Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+"""
+    while True:
+        print(menu)
+        choice = input('Please enter an option: ')
+        if choice == '1':
+            password = input('Please enter your password to encode: ')
+            print(encode(password))
+            print("Your password has been encoded and stored!")
+
+        if choice == '2':
+            decode = input('Please enter an option: ')
+            
+        if choice == '3':
+            exit()
